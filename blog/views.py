9efc8b0f7248d3f35f827e46f7b0e1227d78c49f
python-the-snake <dmitriy-shalimov@yandex.ru>
2020-06-06 09:53:54 +0300

    second portfolio commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
 def blogs(request):
-    return render(request, 'blog/blogs.html')
+    blogs = Blog.objects.order_by('-date')[:5]
+    # limiting seems blogs to 5
+    return render(request, 'blog/blogs.html', {'blogs':blogs})
+
+
